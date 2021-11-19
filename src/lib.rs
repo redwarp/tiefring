7d1e3f35f50dd52936407552a9fa52b36ad83714
Benoit Vermont <redwarp@gmail.com>
2021-11-19 23:31:47 +0100

    Try commit

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ impl Canvas {
                 ops: wgpu::Operations {
                     load: wgpu::LoadOp::Clear(wgpu::Color {
                         r: 0.1,
-                        g: 0.2,
+                        g: 0.3,
                         b: 0.3,
                         a: 1.0,
                     }),
